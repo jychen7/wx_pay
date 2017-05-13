5568526f9bf6a960bb98af4e71c5c013cf10c1cd
Rich Chen <chenjy.rich@gmail.com>
2017-05-13 23:18:35 +0800

    Add sample ssl cert

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,4 +9,8 @@ require 'webmock/minitest'
 WxPay.appid = 'wxd930ea5d5a258f4f'
 WxPay.key = '8934e7d15453e97507ef794cf7b0519d'
 WxPay.mch_id = '1900000109'
-WxPay.debug_mode = false
+WxPay.debug_mode = true
+
+WxPay.apiclient_key = File.read("#{__dir__}/fixtures/files/alice.key")
+WxPay.apiclient_cert = File.read("#{__dir__}/fixtures/files/alice.crt")
+WxPay.set_apiclient_by_pkcs12(File.read("#{__dir__}/fixtures/files/alice.pfx"), "pass")
